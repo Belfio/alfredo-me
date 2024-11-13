9623fa887335cf55b2df4fd0066236cc6ebae45c
albelfio <5812464+Belfio@users.noreply.github.com>
2024-11-13 14:49:48 +0000

    github

--- @/components/Contact.tsx
+++ @/components/Contact.tsx
@@ -1,7 +1,8 @@
 export default function Contact() {
   return (
     <div id="contact" className="my-8">
-      Contact me on <a href="https://x.com/Belfio">twitter</a> or{" "}
+      Contact me on <a href="https://x.com/Belfio">twitter</a>,{" "}
+      <a href="https://github.com/Belfio">github</a> or{" "}
       <a href="https://www.linkedin.com/in/alfredobelfiori/">linkedin</a>.
     </div>
   );
